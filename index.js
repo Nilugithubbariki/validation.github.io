435ae1345a8be025a63720aff99a28ac3301a9ec
Nilugithubbariki <98546192+Nilugithubbariki@users.noreply.github.com>
2022-08-02 13:15:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let result = 1;
+
+
+function validateForm(){
+    if(username.value == ""){
+       document.getElementById("userError").innerHTML = "User Name is Empty"
+       result = 0;
+    }else if(username.value.length < 3){
+        document.getElementById("userError").innerHTML = "User Name required minumum 3 character"
+    }else{
+        document.getElementById("userError").innerHTML = "";
+        result = 1;
+    }
+    if(password.value == ""){
+        document.getElementById("passError").innerHTML = "Password is Empty"
+        result = 0;
+    }else{
+        document.getElementById("passError").innerHTML = "";
+        result = 1;
+    }
+    if(result){
+        return true;
+    }else{
+        return false;
+    }
+}
